797303b7068f86000dfdc6eb4be631bc822c3a4e
Justin Shetty <jas0504@gmail.com>
2016-08-25 02:48:45 -0400

    text conversion functional, working on selection replacement

--- main.js
+++ main.js
@@ -1,29 +1,28 @@
 var temp = null;
-function clickHandle(info){
-	console.log("info: " + JSON.stringify(info));
-	temp = info;
-	// temp = JSON.parse($temp);
-	var text = info["selectionText"];
-	console.log(text);
-	convertText(text);
-	console.log(convertedText);
-}
+var convertedText = "";
 
 window.onload = function(){
 	var context1 = chrome.contextMenus.create({"title": "test", "contexts": ["editable"],
 										    "onclick": clickHandle});
 }
 
-var convertedText = "";
-function convertText(raw){
+function clickHandle(info){
+	// console.log("info: " + JSON.stringify(info));
+	temp = info;
+	var text = info["selectionText"];
+	// console.log(text);
+	convertText(text);
+}
+
+function convertText(raw){ //adds 0xFEE0 (0xFF00 - 0x20) to the input character to shift it into fullwidth
 	convertedText = "";
 	for(var i = 0 ; i < raw.length ; i++){
 		var hexCode = raw.charCodeAt(i);
-		if (hexCode >= 0xFF00 && hexCode <= 0xFFEF) {
-           hexCode =0xFF & (hexCode - 0x20);
+  		if (hexCode > 0x0000 && hexCode <= 0xFF00) {
+           hexCode = 0xFF00 + (hexCode - 0x20);
         }
-        console.log(hecCode);
         convertedText += String.fromCharCode(hexCode);
 	}
+	console.log(convertedText);
 	return convertedText;
 }
